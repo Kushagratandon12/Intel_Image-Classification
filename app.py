91d438d420987343da9c3d4beadf863e0a8ba0e3
Kushagra Tandon <kushagra.tandon.124@gmail.com>
2020-12-30 13:48:39 +0530

    TFLite.Bugs Fix

--- app.py
+++ app.py
@@ -1,35 +1,47 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras.models as Models
+import cv2
+import tflite_runtime.interpreter as tflite
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
-import json
+from flask_cors import CORS
 import os
+
+#  Tensorflow Model Classes 
+classes = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
+#
 
 app=Flask(__name__)
 app.config["IMAGE_UPLOADS"] = "pred_images/"
 cors = CORS(app)
-@cross_origin()
 @app.route('/',methods=['POST','GET'])
 def image_process():
     if request.method == 'GET':
         return 'Send Post Request Here'
+
+    #Request Image Form The Postman
     image = request.files['image']
-    #print(type(image))
     image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-    # img = tf.keras.preprocessing.image.load_img('pred_images/'+image.filename)
-    #image =plt.imread('pred_images/'+image.filename)
-    #plt.imshow(image)
-    classes = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
-    img = tf.keras.preprocessing.image.load_img('pred_images/'+image.filename, target_size=(150,150))
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
-    model = Models.load_model('intel_weights.h5')
-    predictions = model.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
-    #print(predictions)
-    return "This image most likely belongs to {}".format(classes[np.argmax(score)]),200
+    #Pre-Process The Image 
+    image = cv2.imread('pred_images/'+image.filename)
+    img = cv2.resize(image,(160,160))
+    img=img/255.0
+    ##LOAD THE TENSORFLOW LITE MODEL 
+    interpreter = tflite.Interpreter(model_path='tflite_model')
+    interpreter.allocate_tensors()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    # Test model on random input data.
+    input_shape = input_details[0]['shape']
+    input_data = np.array(np.expand_dims(img,0), dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    #print(input_data)
+    interpreter.invoke()
+    output_details = interpreter.get_output_details()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    results = np.squeeze(output_data)
+    #print(results)
+    pred = np.argmax(results)
+    return 'The Image Send Is {}'.format(classes[pred]),200
 
 if __name__ == '__main__':
   app.run(debug=True)
